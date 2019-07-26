3ee8fe65d5e28e249a6e4a8d01ab7d8e080c8967
Edwin Salgado <edwin@devf.mx>
2019-07-25 21:09:31 -0500

    Email Validation

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,7 +1,10 @@
 const UserMutations = require('./User/Mutations');
 const UserQueries = require('./User/Querys');
+const { EmailAddress } = require('graphql-scalars');
+
 
 module.exports = {
+	EmailAddress,
 	Mutation:{
 		...UserMutations, //Split Objects
 	},
